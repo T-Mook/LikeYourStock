0de4cbb2bdfcb55b088ab05d52cc3be72099711f
TMook <msp770@gmail.com>
2019-02-16 14:46:30 +0900

    20190216 PreProcessor Updates 2

--- PreProcessing_stinfo.py
+++ PreProcessing_stinfo.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+import re
 
 ''' Preprocessing Datas '''
 
@@ -20,43 +21,64 @@ def preprocessing_raw_pricedata(code):
     raw_data = pd.read_sql(sql=query, con=conn)
 
     #sorting, del duplicates and resorting
-    raw_data = raw_data.sort_values(by=['날짜'], ascending=True)
-    preprocessed_data = raw_data.drop_duplicates()
+    raw_data = raw_data.sort_values(by=["날짜"], ascending=True)
+    preprocessed_data = raw_data.drop_duplicates(["날짜"])
     preprocessed_data = preprocessed_data.drop(["전일종가"], axis=1)
     preprocessed_data = preprocessed_data.drop(["index"], axis=1)
     preprocessed_data = preprocessed_data.reset_index(drop=True)
     preprocessed_data = preprocessed_data.set_index("날짜")
     
-    callcolumns = ['당일종가', '시가', '고가', '저가', '거래량']
+    callcolumns = ['당일종가', '시가', '고가', '저가', '거래량'] 
     preprocessed_data = preprocessed_data[callcolumns].astype(dtype='int64') #dtype 변경
 
     #Normalization
-    preprocessed_nml_data = preprocessed_data.pct_change(periods=1) #표준화
+    preprocessed_nml_data = preprocessed_data.pct_change(periods=1) #표준화, 전일대비 %로 바꿈
     preprocessed_nml_data = preprocessed_nml_data.dropna(axis=0, how='any')
     
     #Insert code column 
     preprocessed_data.insert(loc=0, column='종목코드', value=code)
     preprocessed_nml_data.insert(loc=0, column='종목코드', value=code)
 
-    result_list = [preprocessed_data, preprocessed_nml_data]
+    #Make new table with normalizaion and Time Series
+    nmlprice_by_date = preprocessed_nml_data.copy(deep=False) #deep=True 시 원본과 복제간 data 연동됨
+    nmlprice_by_date["날짜"] = nmlprice_by_date.index
+    #nmlprice_by_date = nmlprice_by_date.reset_index(drop=True)
+    try:
+        nmlprice_by_date = nmlprice_by_date.pivot(index="종목코드", columns="날짜", values="당일종가")
+    except:
+        print(nmlprice_by_date.head())
+        print(str(code))
+
+    result_list = [preprocessed_data, preprocessed_nml_data, nmlprice_by_date]
 
     return result_list
 
-i = 0
-for code in stock_code['종목코드'][:3]:
-    '''Preprocessing raw data with Nomalization'''
-    table_name1 = str('prestp'+str(code))
-    table_name2 = str('nmlstp'+str(code))
 
-    result_list = preprocessing_raw_pricedata(code)
-    result_list[0].to_sql(name=table_name1, con=conn_save, if_exists = "replace")
-    result_list[1].to_sql(name=table_name2, con=conn_save, if_exists = "replace")
+nmldata_bystock_list = []
+pattern_stcd = re.compile(r"\d{6}")
+i = 0
+for code in stock_code['종목코드'][400:]:
+    '''Preprocessing raw data with Nomalization'''
+    if pattern_stcd.match(code) != None:
+        table_name1 = str('prestp'+str(code))
+        table_name2 = str('nmlstp'+str(code))
     
-    i += 1
-    if i % 100 == 0:
-        print(str(i) + " stocks are preprocessed.")
+        result_list = preprocessing_raw_pricedata(code)
+        result_list[0].to_sql(name=table_name1, con=conn_save, if_exists="replace")
+        result_list[1].to_sql(name=table_name2, con=conn_save, if_exists="replace")
+        nmldata_bystock_list.append(result_list[2]) #list 인자로 종목별 dataframe 전달
+      
+        i += 1
+        if i % 100 == 0:
+            print(str(i) + " stocks are preprocessed.")
+        else:
+            pass
     else:
+        print(str(code))
         pass
+
+nmldata_bystock_df = pd.concat(nmldata_bystock_list, axis=0) #list 인자인 dataframe 합치기
+nmldata_bystock_df.to_sql(name="tsnmp00", con=conn_save, if_exists="replace") #Time Sereis Normalization Price in Kospi(00)
 
 print(str(i) + " stocks are preprocessed.")
 print("Done.")
