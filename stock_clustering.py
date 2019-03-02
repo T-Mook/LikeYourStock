b3ba8b0c5cdf47e4a9115c1e403989b3cb7c962d
TMook <msp770@gmail.com>
2019-03-02 19:21:44 +0900

    20190302 PCA 1차 테스트 (Mac)

--- stock_clustering.py
+++ stock_clustering.py
@@ -1,6 +1,43 @@
+import matplotlib as mpl; mpl.use('TkAgg') #only for MAC OS 
+import matplotlib.pyplot as plt
+#import mglearn
 import sqlite3
 import pandas as pd
-import sklearn
+from sklearn import decomposition
 
-conn = sqlite3.connect('d:/Stocks/ppkospi91d.db') #Preprocessed Raw data
+dataPathInMac = "/Users/tmook/documents/Projects/LikeYourStock/Stocks/ppkospi91d.db" #Mac 경로
+dataPathInWin = "d:/Stocks/ppkospi91d.db"
+
+conn = sqlite3.connect(dataPathInMac) #Preprocessed Raw data
 cur = conn.cursor()
+
+tableName = "tsnmp00"
+query = "SELECT * FROM "+str(tableName)
+sampleData = pd.read_sql(sql=query, con=conn)
+
+#name, features로 쪼개기
+sampleName = sampleData['종목코드']
+sampleFetures = sampleData[sampleData.columns[1:]]
+sampleNameNdarray = sampleName.values
+sampleFeturesNdarray = sampleFetures.values
+
+X = sampleFeturesNdarray
+
+#part1 : PCA
+pca = decomposition.PCA(n_components=6)
+pca.fit(X)
+XTrans = pca.transform(X)
+
+#PCA check
+for i in range(0, 5): # 전체는 X.shape[1]
+    plt.subplot(2, 1, 1)
+    plt.plot(X[i])
+    plt.title("original")
+
+    plt.subplot(2, 1, 2)
+    plt.plot(XTrans[i])
+    plt.title("After PCA")
+
+plt.show()
+
+conn.close() #sqlite3 연결 종료문
---
+++ testpage.py
@@ -0,0 +1,6 @@
+import matplotlib as mpl; mpl.use('TkAgg') #only for MAC OS 
+import matplotlib.pyplot as plt
+import mglearn
+
+mglearn.plots.plot_kmeans_algorithm()
+plt.show()
