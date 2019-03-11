ede3cc585eff5030d2a3dbd425c3490936d378e1
TMook <msp770@gmail.com>
2019-03-12 00:43:07 +0900

    190312 파일정리 및 clustering 적용 시작

--- 04_stock_clustering.py
+++ 04_stock_clustering.py
@@ -1,8 +1,9 @@
 import sqlite3
 import matplotlib as mpl; mpl.use('TkAgg') #only for MAC OS 
 import matplotlib.pyplot as plt
-#import mglearn
 import pandas as pd
+
+from sklearn.cluster import KMeans
 from sklearn import decomposition
 
 dataPathInMac = "/Users/tmook/documents/Projects/LikeYourStock/Stocks/ppkospi91d.db" #Mac 경로
@@ -30,18 +31,25 @@ pca = decomposition.PCA(n_components=4)
 pca.fit(X)
 XTrans = pca.transform(X)
 
-#PCA check
-for i in range(0, 3): # 전체는 X.shape[1]
-    plt.subplot(2, 1, 1)
-    plt.plot(X[i], label=str(sampleNameNdarray[i]))
-    plt.title("original")
-    plt.legend(loc=3)
+# #PCA check
+# for i in range(0, 3): # 전체는 X.shape[1]
+#     plt.subplot(2, 1, 1)
+#     plt.plot(X[i], label=str(sampleNameNdarray[i]))
+#     plt.title("original")
+#     plt.legend(loc=3)
+# 
+#     plt.subplot(2, 1, 2)
+#     plt.plot(XTrans[i], label=str(sampleNameNdarray[i]))
+#     plt.title("After PCA (n_components=4)")
+#     plt.legend(loc=3)
+# 
+# plt.show()
 
-    plt.subplot(2, 1, 2)
-    plt.plot(XTrans[i], label=str(sampleNameNdarray[i]))
-    plt.title("After PCA")
-    plt.legend(loc=3)
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(XTrans)
 
-plt.show()
+print("Label Count: {}".format(len(kmeans.labels_)))
+print("Cluster Label:\n{}".format(kmeans.labels_))
+
 
 conn.close() #sqlite3 연결 종료문
